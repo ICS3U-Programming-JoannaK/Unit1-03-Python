41b0a72f3d5e228c734790dfbe810e0662b03e9b
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-02-13 07:28:42 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created By: Joanna Keza
+# Date: February 12, 2025
+# This program displays Hello, World! with style
+
+
+def main():
+    print("Hello, World! with style")
+
+
+if __name__ == "__main__":
+    main()
